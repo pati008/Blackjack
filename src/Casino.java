246c4c99c78966a35693567ffc1bb35c4b09f768
pati008 <patrycja_pogorzelska26@milton.edu>
2024-05-06 17:54:30 -0400

    I set up variables for the player and card.

---
+++ src/Casino.java
@@ -0,0 +1,12 @@
+public class Casino {
+    public static void main(String[] args) {
+        Casino c = new Casino();
+    }
+
+    public Casino(){
+        System.out.println("welcome to Pati's Casino!");
+
+
+    }
+
+}
---
+++ src/Player.java
@@ -0,0 +1,13 @@
+public class Player {
+
+    public int currentValue;
+    public String whatCards;
+    public String playerName;
+    public boolean player;
+    public boolean hit;
+    public boolean stay;
+
+
+
+
+}
