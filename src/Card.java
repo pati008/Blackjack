332b29072bdbd562cb73c01f3389e639e6544a09
pati008 <patrycja_pogorzelska26@milton.edu>
2024-05-07 12:20:14 -0400

    I set up variables for the player and card.

---
+++ src/Card.java
@@ -0,0 +1,22 @@
+public class Card {
+
+
+
+    public boolean heart;
+    public boolean club;
+    public boolean spade;
+    public boolean diamond;
+
+    public int value;
+
+    public int cardNumber;
+
+    public boolean Jack;
+    public boolean Queen;
+    public boolean King;
+    public boolean Ace;
+    public boolean numberCard;
+
+
+
+}
