293ebf3af52eaa87a76625142c3f574477064331
pati008 <patrycja_pogorzelska26@milton.edu>
2024-05-11 14:32:29 -0400

    hw due 5/11

--- src/Player.java
+++ src/Player.java
@@ -1,19 +1,19 @@
 public class Player {
 
     public int currentValue;
-
-
-
     public Card []hand;
-
-
-
     public String playerName;
     public boolean isPlayer;
     public boolean hit;
     public boolean stay;
 
 
+    public Player (int pcurrentValue, Card pHand, String pPlayerName, boolean pIsPlayer, boolean pHit, boolean pStay){
 
+    }
+
+    public void print(){
+        System.out.println("Hi " + playerName+ "! I see that it is  " + isPlayer +" that you are a player." + playerName+ "you currently hold " + hand +" with the value of " + currentValue);
+    }
 
 }
